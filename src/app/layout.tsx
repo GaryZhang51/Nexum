8ecb2d55a4158816a00f6e89d050b6b45e360858
Daniel Zhang <d.zhang200788@gmail.com>
2024-02-08 22:54:13 -0500

    feat: sidebar frontend

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
-import "./globals.css";
+import "./globals.scss";
 import "@radix-ui/themes/styles.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const ibmPlexSans = IBM_Plex_Sans({
+    weight: ["400", "600", "700"],
+    subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
     title: "Nexum",
@@ -18,8 +21,8 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <Theme>{children}</Theme>
+            <body className={ibmPlexSans.className}>
+                <Theme accentColor="teal">{children}</Theme>
             </body>
         </html>
     );
