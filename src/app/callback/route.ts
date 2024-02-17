bcb33f9b4391a71458039eeeef5fd6552f8c186e
Daniel Zhang <d.zhang200788@gmail.com>
2024-02-16 23:51:46 -0500

    feat: implement next-rest-framework

--- src/app/callback/route.ts
+++ src/app/callback/route.ts
@@ -1,58 +1,91 @@
 import { SignJWT } from "jose";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getJwtSecretKey, workos, getClientId } from "../../auth";
+import { route, routeOperation } from "next-rest-framework";
+import { z } from "zod";
 
-export async function GET(request: NextRequest) {
-  const code = request.nextUrl.searchParams.get("code");
-  console.log(code === null)
-  if (code) {
-    
-    try {
-      // Use the code returned to us by AuthKit and authenticate the user with WorkOS
-      const { user } = await workos.userManagement.authenticateWithCode({
-        clientId: getClientId(),
-        code,
-      });
+export const { GET } = route({
+    getJWT: routeOperation({
+        method: "GET",
+    })
+        .outputs([
+            {
+                status: 200,
+                contentType: "application/json",
+                schema: z.any(),
+            },
+            {
+                status: 500,
+                contentType: "application/json",
+                schema: z.object({
+                    error: z.string(),
+                }),
+            },
+            {
+                status: 400,
+                contentType: "application/json",
+                schema: z.object({
+                    error: z.string(),
+                }),
+            },
+        ])
+        .handler(async (request) => {
+            const code = request.nextUrl.searchParams.get("code");
+            console.log(code === null);
+            if (code) {
+                try {
+                    // Use the code returned to us by AuthKit and authenticate the user with WorkOS
+                    const { user } =
+                        await workos.userManagement.authenticateWithCode({
+                            clientId: getClientId(),
+                            code,
+                        });
 
-      // Create a JWT token with the user's information
-      const token = await new SignJWT({
-        // Here you might lookup and retrieve user details from your database
-        user,
-      })
-        .setProtectedHeader({ alg: "HS256", typ: "JWT" })
-        .setIssuedAt()
-        .setExpirationTime("1h")
-        .sign(getJwtSecretKey());
+                    // Create a JWT token with the user's information
+                    const token = await new SignJWT({
+                        // Here you might lookup and retrieve user details from your database
+                        user,
+                    })
+                        .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+                        .setIssuedAt()
+                        .setExpirationTime("1h")
+                        .sign(getJwtSecretKey());
 
-      const url = request.nextUrl.clone();
+                    const url = request.nextUrl.clone();
 
-      // Cleanup params
-      url.searchParams.delete("code");
+                    // Cleanup params
+                    url.searchParams.delete("code");
 
-      // Redirect to the requested path and store the session
-      url.pathname = "/";
-      const response = NextResponse.redirect(url);
+                    // Redirect to the requested path and store the session
+                    url.pathname = "/";
+                    const response = NextResponse.redirect(url);
 
-      response.cookies.set({
-        name: "token",
-        value: token,
-        path: "/",
-        httpOnly: true,
-        secure: true,
-        sameSite: "lax",
-      });
+                    response.cookies.set({
+                        name: "token",
+                        value: token,
+                        path: "/",
+                        httpOnly: true,
+                        secure: true,
+                        sameSite: "lax",
+                    });
 
-      return response;
-    } catch (error) {
-      const errorRes = {
-        error: error instanceof Error ? error.message : String(error),
-      };
-      console.error(errorRes);
-      return NextResponse.json(errorRes);
-    }
-  }
+                    return response;
+                } catch (error) {
+                    const errorRes = {
+                        status: 500,
+                        error:
+                            error instanceof Error
+                                ? error.message
+                                : String(error),
+                    };
+                    console.error(errorRes);
+                    return NextResponse.json(errorRes);
+                }
+            }
 
-  return NextResponse.json({
-    error: "No authorization code was received from AuthKit",
-  });
-}
+            return NextResponse.json({
+                status: 400,
+                error: "No authorization code was received from AuthKit",
+            });
+        }),
+});
